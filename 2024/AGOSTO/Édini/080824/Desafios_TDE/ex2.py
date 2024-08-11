40927c57604530cd1bad24d04ff6a31a5d1f7e65
Davi <samuraiblack08@gmail.com>
2024-08-11 19:29:18 -0400

    Commit Add
    Atividade 02 feita.

--- 2024/AGOSTO/Édini/080824/Desafios_TDE/ex2.py
+++ 2024/AGOSTO/Édini/080824/Desafios_TDE/ex2.py
@@ -2,4 +2,35 @@
 
 # Crie uma exceção personalizada (subclasse de Exception) com uma mensagem específica. Use essa exceção em um contexto relevante.​
 
-# Em andamento
+class CompraInvalida(Exception):
+    def init(self,mensagem):
+        super().init(mensagem)
+class Compra:
+    def init(self, valor):
+        if valor < 0:
+            raise CompraInvalida("Compra não aprovada\nFavor conderir valor!! ")
+        self.valor = valor
+        self.taxa = self.calcular_taxa()
+        self.total = self.valor + self.taxa
+ 
+    def calcular_taxa(self):
+        if self.valor >=50:
+            return self.valor * 0.20
+        else:
+            return self.valor * 0.10
+ 
+    def comprovante(self):
+        print("\n---Comprovante de Compra---")
+        print(f"Valor da compra: R$ {self.valor:.2f}")
+        print(f"Valor da taxa: R$ {self.taxa:.2f}")
+        print(f"Total a pagar: R$ {self.total:.2f}")
+while True:
+    try:
+        valor = float(input("Digite o valor da sua compra: "))
+        compra = Compra(valor)
+        compra.comprovante()
+        break
+    except CompraInvalida as e:
+        print(f"Erro: {e}")
+    except ValueError:
+        print("Entrada inválida.por favor, digite um número.")
