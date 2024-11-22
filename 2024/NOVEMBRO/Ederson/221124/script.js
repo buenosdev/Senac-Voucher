00025e318818d28ea432a5cc1bf88e5248f77909
Davi <samuraiblack08@gmail.com>
2024-11-22 10:11:27 -0400

    Commit Add
    Aula de hoje

---
+++ 2024/NOVEMBRO/Ederson/221124/script.js
@@ -0,0 +1,36 @@
+// window.confirm("Confirma a conclusão?");
+
+// window.alert("Olá mundo!");
+
+// window.prompt("Qual seu nome?") // Recebe uma string
+
+// console.log - para consultar variavel
+
+// var x;
+// const = 3.14;
+// let y;
+// x=5;
+// y=6;
+// let z=x;
+// let w=10;
+
+
+// var nome = window.prompt("Qual seu nome?")
+// window.alert("Olá " + nome + ", Seja bem vindo")
+
+// var media = 8;
+
+// media >=7 ? 'Aprovao' :'Reprovado';
+
+// Document.write(media);
+
+// function eita(p1,p2) {
+//     return p1* p2
+// }
+
+// // chamada
+
+// eita(10,20)
+
+
+
